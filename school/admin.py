bf2221a1cc1a956b5da57327e379cd0841e4c47c
Silas010 <rerimoisilas@gmail.com>
2023-11-29 15:54:59 +0300

    my project

---
+++ school/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(StudentExtra)
+admin.site.register(TeacherExtra)
+admin.site.register(Attendance)
+admin.site.register(Notice)
